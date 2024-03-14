dac111c4725f35c83ffa3f5a1558b410837ae57d
jjoonior <jjoonior97@gmail.com>
2024-03-14 17:20:58 +0900

    Feat: contentsImage / profileImage entity 생성

---
+++ src/entity/contentsImage.entity.ts
@@ -0,0 +1,20 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ContentsTypeEnum } from './contentsType.enum';
+
+@Entity('contentsImage')
+export class ContentsImageEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  key: string;
+
+  @Column()
+  url: string;
+
+  @Column()
+  contentsType: ContentsTypeEnum;
+
+  @Column()
+  contentsId: number;
+}
