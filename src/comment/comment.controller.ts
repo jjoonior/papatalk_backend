9e4a7f84d3a785bd6de182e8cb950ffea4168672
jjoonior <jjoonior97@gmail.com>
2024-03-14 10:28:21 +0900

    Feat: 댓글 추천 api swagger 적용

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -3,34 +3,91 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Post,
   Put,
   Query,
   Req,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { CommentService } from './comment.service';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { AuthGuard } from '../auth/guard/auth.guard';
 import { CreateCommentReqDto } from './dto/createCommentReq.dto';
 import { ValidateUserGuard } from '../auth/guard/validateUser.guard';
 import { UpdateCommentReqDto } from './dto/updateCommentReq.dto';
+import { GetCommentListResDto } from './dto/getCommentListRes.dto';
 
 @Controller('/community/:communityId/comments')
 @ApiTags('Community Comment')
+@ApiInternalServerErrorResponse({
+  schema: {
+    example: {
+      statusCode: 500,
+      message: 'Internal server error',
+    },
+  },
+})
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Get()
   @UseGuards(ValidateUserGuard)
+  @ApiOperation({
+    summary: '커뮤니티 댓글 목록 조회',
+    description: '커뮤니티 댓글 목록 조회',
+  })
+  @ApiParam({
+    name: 'communityId',
+    description: '커뮤니티 게시글 id',
+    example: 1,
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'page',
+    description: '페이지',
+    example: 1,
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'sort',
+    description: '정렬 (최신순/추천순)',
+    example: 'createdAt',
+    required: false,
+    type: String,
+  })
+  @ApiOkResponse({ type: GetCommentListResDto })
+  @ApiNotFoundResponse({
+    description: '',
+    schema: {
+      example: {
+        message: '존재하지 않는 글입니다.',
+        error: 'NotFound',
+        statusCode: 404,
+      },
+    },
+  })
   async getCommentList(
     @Param('communityId') contentsId: number,
     @Query('page') page = 1,
     @Query('sort') sort = 'createdAt',
     @Query('take') take = 10,
     @Req() req,
-  ) {
+  ): Promise<GetCommentListResDto> {
     const [commentList, totalCount] = await this.commentService.getCommentList(
       contentsId,
       page,
@@ -75,6 +132,27 @@ export class CommentController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: '커뮤니티 댓글 생성',
+    description: '커뮤니티 댓 생성',
+  })
+  @ApiCreatedResponse()
+  @ApiParam({
+    name: 'communityId',
+    description: '커뮤니티 게시글 id',
+    example: 1,
+    type: Number,
+  })
+  @ApiNotFoundResponse({
+    description: '',
+    schema: {
+      example: {
+        message: '존재하지 않는 글입니다.',
+        error: 'NotFound',
+        statusCode: 404,
+      },
+    },
+  })
   async createComment(
     @Param('communityId') contentsId: number,
     @Body() dto: CreateCommentReqDto,
@@ -85,6 +163,43 @@ export class CommentController {
 
   @Put(':commentId')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: '커뮤니티 댓글 수정',
+    description: '커뮤니티 댓글 수정',
+  })
+  @ApiOkResponse()
+  @ApiParam({
+    name: 'communityId',
+    description: '커뮤니티 게시글 id',
+    example: 1,
+    type: Number,
+  })
+  @ApiParam({
+    name: 'commentId',
+    description: '커뮤니티 댓글 id',
+    example: 1,
+    type: Number,
+  })
+  @ApiUnauthorizedResponse({
+    description: '',
+    schema: {
+      example: {
+        message: '권한이 없습니다.',
+        error: 'Unauthorized',
+        statusCode: 401,
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: '',
+    schema: {
+      example: {
+        message: '존재하지 않는 글입니다.',
+        error: 'NotFound',
+        statusCode: 404,
+      },
+    },
+  })
   async updateComment(
     @Param('communityId') contentsId: number,
     @Param('commentId') commentId: number,
@@ -95,19 +210,96 @@ export class CommentController {
 
   @Delete(':commentId')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: '커뮤니티 댓글 삭제',
+    description: '커뮤니티 댓글 삭제',
+  })
+  @ApiOkResponse()
+  @ApiParam({
+    name: 'communityId',
+    description: '커뮤니티 게시글 id',
+    example: 1,
+    type: Number,
+  })
+  @ApiParam({
+    name: 'commentId',
+    description: '커뮤니티 댓글 id',
+    example: 1,
+    type: Number,
+  })
+  @ApiUnauthorizedResponse({
+    description: '',
+    schema: {
+      example: {
+        message: '권한이 없습니다.',
+        error: 'Unauthorized',
+        statusCode: 401,
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: '',
+    schema: {
+      example: {
+        message: '존재하지 않는 글입니다.',
+        error: 'NotFound',
+        statusCode: 404,
+      },
+    },
+  })
   async deleteComment(
     @Param('communityId') contentsId: number,
     @Param('commentId') commentId: number,
   ) {
     await this.commentService.deleteComment(contentsId, commentId);
+    return;
   }
 
   @Post(':commentId/like')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: '커뮤니티 댓글 추천 토글',
+    description: '커뮤니티 추천 토글',
+  })
+  @ApiParam({
+    name: 'communityId',
+    description: '커뮤니티 게시글 id',
+    example: 1,
+    type: Number,
+  })
+  @ApiParam({
+    name: 'commentId',
+    description: '커뮤니티 댓글 id',
+    example: 1,
+    type: Number,
+  })
+  @ApiCreatedResponse({ description: '추천 true' })
+  @ApiNoContentResponse({ description: '추천 false' })
+  @ApiUnauthorizedResponse({
+    description: '',
+    schema: {
+      example: {
+        message: '권한이 없습니다.',
+        error: 'Unauthorized',
+        statusCode: 401,
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: '',
+    schema: {
+      example: {
+        message: '존재하지 않는 글입니다.',
+        error: 'NotFound',
+        statusCode: 404,
+      },
+    },
+  })
   async toggleCommentLike(
     @Param('communityId') contentsId: number,
     @Param('commentId') commentId: number,
     @Req() req,
+    @Res() res,
   ) {
     const liked = await this.commentService.toggleCommentLike(
       req.user,
@@ -115,6 +307,10 @@ export class CommentController {
       commentId,
     );
 
-    return { liked };
+    if (liked) {
+      return res.status(HttpStatus.CREATED).send();
+    } else {
+      return res.status(HttpStatus.NO_CONTENT).send();
+    }
   }
 }
--- src/comment/dto/updateCommentReq.dto.ts
+++ src/comment/dto/updateCommentReq.dto.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateCommentReqDto {
+  @ApiProperty({
+    description: '댓글 내용',
+    type: String,
+    required: true,
+    example: '예시_댓글',
+  })
   content: string;
 }
